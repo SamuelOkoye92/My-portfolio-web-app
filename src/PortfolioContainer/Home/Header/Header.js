ea8ec1f06106627881c51e1312070855d2819450
Samuel okoye <samuelokoye92@gmail.com>
2024-01-27 22:46:52 +0100

    update

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -52,7 +52,14 @@ export default function Header() {
 
     return (
         <div className='header-option' onClick={() => setShowHeaderOptions(!showHeaderOptions)}>
-            
+            <div className='header-parent'>
+                <div className='header-hamburger' onClick={()=> setShowHeaderOptions(!showHeaderOptions)}>
+                    <FontAwesomeIcon className='header-hamburger-bars' icon={faBars} />
+                </div>
+                <div className='header-logo'>
+                    <span></span>
+                </div>
+            </div>
         </div>
     );
 };
